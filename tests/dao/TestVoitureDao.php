1b73baf73276d893ed3fa6a7e2464b7c459054d1
lukostelloe <lukostelloe@gmail.com>
2021-10-22 13:57:21 +0200

    tests working

--- tests/dao/TestVoitureDao.php
+++ tests/dao/TestVoitureDao.php
@@ -16,12 +16,12 @@ class TestVoitureDao extends TestCase {
 
     public static function tearDownAfterClass () : void
     {
-        $voiture = TestVoitureDao::$voitureDao->findById (4);
-        $voiture->setCouleur ("champagne");
+        $voiture = TestVoitureDao::$voitureDao->findById (1);
+        $voiture->setCouleur ("White");
         TestVoitureDao::$voitureDao->updateVoiture($voiture);
-        TestVoitureDao::$voitureDao->insertVoiture2(53, "Blanc", "Citroen", "AX", "TR-654-WL");
+        TestVoitureDao::$voitureDao->insertVoiture2(3, "ab-789-cd", "Blue", "VW", "Golf");
+        TestVoitureDao::$voitureDao->deleteVoiture(4);
     }
-
 
     public function testFindAll () {
         $voitures = TestVoitureDao::$voitureDao->findAll();
@@ -29,26 +29,26 @@ class TestVoitureDao extends TestCase {
     }
 
     public function testfindById () {
-        $voiture = TestVoitureDao::$voitureDao->findById (4);
+        $voiture = TestVoitureDao::$voitureDao->findById (2);
         $this->assertNotNull($voiture);
     }
 
     public function testUpdateVoiture () {
-        $voiture = TestVoitureDao::$voitureDao->findById (4);
-        $voiture->setCouleur ("rouge");
+        $voiture = TestVoitureDao::$voitureDao->findById (1);
+        $voiture->setCouleur ("Red");
         TestVoitureDao::$voitureDao->updateVoiture($voiture);
-        $voitureUpdated = TestVoitureDao::$voitureDao->findById (4);
-        $this->assertEquals($voitureUpdated->getCouleur(),"rouge");
+        $voitureUpdated = TestVoitureDao::$voitureDao->findById (1);
+        $this->assertEquals($voitureUpdated->getCouleur(),"Red");
     }
 
     public function testDeleteVoiture () {
-        $isDeleted = TestVoitureDao::$voitureDao->deleteVoiture (53);
+        $isDeleted = TestVoitureDao::$voitureDao->deleteVoiture (3);
         $this->assertTrue ($isDeleted);
     }
 
 
-    public function testInsertVoiture(){
-        $isInserted = TestVoitureDao::$voitureDao->insertVoiture (4);
-        $this->assertTrue ($isInserted);
+    public function testInsertVoiture2(){
+        $isInserted = TestVoitureDao::$voitureDao->insertVoiture2 ("4","xx-555-yy","Green","Volvo","A5");
+        $this->assertNotNull ($isInserted);
     }
 }
